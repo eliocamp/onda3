1409a153c64de4c40bf588b45aecdab898c977d6
Unknown <eliocampitelli@gmail.com>
2018-11-23 13:39:19 -0300

    aao zonal

--- zonal.aao.R
+++ zonal.aao.R
@@ -3,6 +3,14 @@ library(data.table)
 library(ggplot2)
 library(magrittr)
 library(ggperiodic)
+library(here)
+source(here("scripts/helperfun.R"))
+
+map <- fortify(map_data("world2")) %>% 
+   subset(lat < -20) %>% 
+   geom_path(data = ., aes(long, lat, group = group), size = 0.3)
+
+## Leo datos ----
 
 hgt <- ReadNetCDF("~/DATOS/NCEP Reanalysis/hgt.mon.mean.nc", "hgt", 
            subset = list(level = 700, 
@@ -11,119 +19,82 @@ hgt <- ReadNetCDF("~/DATOS/NCEP Reanalysis/hgt.mon.mean.nc", "hgt",
    setnames("level", "lev") %>% 
    .[, hgt.a := Anomaly(hgt), by = .(lon, lat, month(time))]
 
-copy(hgt) %>%  
-   .[, hgt := hgt.a*sqrt(cos(lat*pi/180)), by = .(lon, lat, month(time))] %>% 
-   EOF(hgt ~ lon + lat | time, 1, data = .) -> aao
-   
-   
-ggplot(aao$left, aes(lon, lat)) +
-   geom_contour_fill(aes(z = hgt)) +
-   coord_polar(start = 180*pi/180) +
-   scale_fill_divergent() 
-
-sam.pattern <- aao$right[, .(time, aao = hgt)] %>% 
-   .[, aao := aao/sd(aao)] %>% 
-   .[hgt, on = "time"] %>% 
-   .[, FitLm(hgt, aao), by = .(lon, lat)] %>% 
-   .[term == "aao"]
-
-ggplot(sam.pattern, aes(lon, lat)) +
-   geom_contour_fill(aes(z = -estimate), breaks = seq(-50, 20, by = 5)) +
-   coord_polar(start = 180*pi/180) +
-   scale_fill_divergent() 
-
 oficial <- fread("http://www.cpc.ncep.noaa.gov/products/precip/CWlink/daily_ao_index/aao/monthly.aao.index.b79.current.ascii") %>% 
    .[, time := lubridate::ymd_h(paste0(V1, "-", V2, "-01:00"))] %>% 
    .[, c("V1", "V2") := NULL] %>% 
    setnames("V3", "oficial")
 
-
-
-
-aao$right %>% 
-   .[oficial, on = "time"] %>% 
-   ggplot(aes(oficial, -hgt/sd(hgt, na.rm = TRUE))) +
-   geom_point()
+# Dividiendo la AAO ----
 
 copy(hgt) %>% 
    # .[, hgt := Anomaly(hgt), by = .(lon, lat, month(time))] %>%
-   .[, .(hgt = mean(hgt.a)*sqrt(cos(lat*pi/180))), by = .(lat, time)] %>%  
-   EOF(hgt ~ lat | time, 1, data = .) -> zonal.eof
+   .[, aao := hgt.a*sqrt(cos(lat*pi/180))] %>%  
+   EOF(aao ~ lon + lat | time, 1, data = .) -> aao
 
-ggplot(zonal.eof$left, aes(lat, hgt)) +
-   geom_line(aes(color = PC))
+aao$left[, sim := mean(aao), by = .(lat)]
+aao$left[, asim := Anomaly(aao), by = .(lat)]
 
-zonal.eof$right %>% 
-   .[oficial, on = "time"] %>% 
-   .[complete.cases(.)] %>% 
-   ggplot(aes(-hgt/sd(hgt), oficial)) +
-   geom_point()
-
-zonal.pred <- predict(zonal.eof)
-
-hgt[, mean(hgt.a), by = .(lat, time)] %>% 
-   .[zonal.pred, on = c("lat", "time")] %>% 
-   .[time == time[1]] %>% 
-   melt(id.vars = c("lat", "time")) %>% 
-   ggplot(aes(lat, value)) +
-   geom_line(aes(color = variable))
-
-zonal.pred[, .(lat, time, aaoz = hgt)] %>% 
-   .[hgt, on = c("lat", "time")] %>%
-   .[, hgt.aao := hgt.a - aaoz] -> hgt
-
-
-copy(hgt) %>% 
-   .[, hgt.aao := hgt.aao*sqrt(cos(lat*pi/180))] %>% 
-   EOF(hgt.aao ~ lon + lat | time, 1:5, data = .) -> asim.eof
-
-periodic(asim.eof$left, lon = c(0, 360)) %>% 
-   ggplot(aes(lon, lat)) +
-   geom_contour_fill(aes(z = -hgt.aao)) +
-   facet_wrap(~PC) +
+ggplot(aao$left, aes(lon, lat)) +
+   geom_contour_fill(aes(z = asim)) +
+   geom_contour(aes(z = sim)) + 
    coord_polar()
 
-zonal.eof$right[, .(time, zonal.eof = hgt)] %>% 
-   .[hgt, on = "time"] %>% 
-   .[, cor(hgt.a, zonal.eof), by = .(lon, lat)] %>% 
+hgt2 <- aao$left[, .(lon, lat, sim, asim)][hgt, on = c("lon", "lat")]
+
+
+# Las aaos son los coeficientes de la regresión múltiple de la altura 
+# geopotential y cada patrón. 
+
+aaos <- ReadNetCDF("~/DATOS/NCEP Reanalysis/hgt.mon.mean.nc", "hgt", 
+                  subset = list(level = 700, 
+                                lat = c(-90, -20))) %>% 
+   setnames("level", "lev") %>% 
+   .[, hgt.a := Anomaly(hgt), by = .(lon, lat, month(time))] %>% 
+   .[aao$left[, .(lon, lat, sim, asim)], on = c("lon", "lat")] %>% 
+   .[, FitLm(-hgt.a, sim, asim), by = .(time)] %>% 
+   .[term != "(Intercept)"] %>% 
+   .[, estimate := (estimate - mean(estimate))/sd(estimate), by = term] 
+
+oficial[aaos, on = "time"] %>% 
+   ggplot(aes(oficial, estimate)) +
+   geom_point(aes(color = term))
+
+oficial[aaos, on = "time"] %>% 
+   .[, cor(oficial, estimate, use = "complete.obs"), by = term]
+
+
+# Efectos ----
+aaos <- dcast(aaos, time ~ term, value.var = "estimate") 
+
+temp <- ReadNetCDF(here::here("DATA/NCEP Reanalysis/air.mon.mean.nc"), c(t = "air"),
+                   subset = list(level = 700, 
+                                 lat = c(-90, -20),
+                                 time = as.character(range(aaos$time)))) %>% 
+   .[, t.a := Anomaly(t), by = .(lon, lat, month(time))]
+
+aaos[temp, on = "time"] %>% 
+   .[, FitLm(t.a, asim, sim), by = .(lon, lat)] %>%
+   .[term != "(Intercept)"] %>% 
+   ggperiodic::periodic(lon = c(0, 360)) %>% 
+   # dcast(lon + lat ~ term, value.var = "estimate") %>% 
    ggplot(aes(lon, lat)) +
-   geom_contour_fill(aes(z = V1)) +
+   geom_contour_fill(aes(z = estimate), breaks = AnchorBreaks(0, NULL, 0)) +
+   map +
    scale_fill_divergent() +
-   coord_polar()
+   annotate("point", 310, -65) +
+   coord_polar() +
+   facet_wrap(~term)
 
-sams <- sam.pattern[, .(lon, sam.zonal = mean(estimate),
-                sam.asim = Anomaly(estimate)), by = .(lat)] 
-ggplot(sams, aes(lon, lat)) +
-   geom_contour_fill(aes(z = sam.asim)) +
-   geom_contour2(aes(z = sam.zonal)) +
-   coord_polar()
-
-ggplot(sams, aes(lon, lat)) +
-   geom_contour_fill(aes(z = sam.asim + sam.zonal)) +
-   # geom_contour2(aes(z = sam.zonal)) +
-   coord_polar()
-
-sams.indexes <- sams[hgt, on = c("lat", "lon")] %>% 
-   .[, .(zonal = cor(hgt, sam.zonal),
-         asim = cor(hgt, sam.asim)), by = .(time)]
-
-sams.indexes %>% 
-   melt(id.vars = "time") %>% 
-   ggplot(aes(time, value)) +
-   geom_line(aes(color = variable))
-
-sams.indexes %>% 
-   .[oficial, on = "time"] %>% 
-   .[complete.cases(.)] %>% 
-   lm(oficial ~ zonal*asim, data = .) -> model
-
-
-
-   melt(id.vars = "time") %>% 
-   .[oficial, on = "time"] %>% 
-   .[complete.cases(.)] %>% 
+(aaos[temp[lat %~% -65 & lon %~% 310], on = "time"] %>% 
+   ggplot(aes(time, t.a)) +
+   geom_line() +
+   geom_line(aes(y = asim), color = "red") +
+      geom_line(aes(y = sim), color = "blue")) %>% 
    
-   .[, cor(value, oficial), by = variable]
-   ggplot(aes(oficial, value)) +
-   geom_point(aes(color = variable))
-   
+   ggwrap::ggwrap(2)
+
+aaos[temp[lat %~% -65 & lon %~% 310], on = "time"] %>% 
+   .[, cor := gtools::running(t.a, asim, cor, 11, pad = TRUE)] %>% 
+   ggplot(aes(time, cor)) +
+   geom_line() +
+   geom_smooth()
