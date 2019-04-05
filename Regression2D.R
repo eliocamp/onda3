097c5347accc5634ba754979ea731db6201d1317
Unknown <eliocampitelli@gmail.com>
2019-04-05 17:51:06 -0300

    Más de regresión 2d

--- Regression2D.R
+++ Regression2D.R
@@ -1,14 +1,5 @@
-library(ggplot2)
-library(metR)
-library(data.table)
-library(magrittr)
 
-world <- subset(map_data("world2"), lat > 0)
-
-geom_world <- geom_path(data = world, aes(long, lat, group = group),
-                        size = 0.2)
-
-Regression2D <- function(formula, data = NULL, B = 1, seed = 42) {
+enrich_formula <- function(formula) {
    f <- as.character(formula)
    f <- stringr::str_split(f,"~", n = 2)[[1]]
    
@@ -20,180 +11,159 @@ Regression2D <- function(formula, data = NULL, B = 1, seed = 42) {
    row.vars <- stringr::str_squish(stringr::str_split(matrix.vars[1], "\\+")[[1]])
    col.vars <- stringr::str_squish(stringr::str_split(matrix.vars[2], "\\+")[[1]])
    
+   dcast.formula <- stringr::str_squish(f[stringr::str_detect(f, "\\|")])
+   dcast.formula <- as.formula(stringr::str_replace(dcast.formula, "\\|", "~"))
+   value.var <- stringr::str_squish(f[!stringr::str_detect(f, "\\|")])
+   
+   return(list(formula = formula,
+               dims = dcast.formula, 
+               value.var = value.var,
+               row.vars = row.vars, 
+               col.vars = col.vars))
+}
+
+data_from_formula <- function(formula, data) {
    if (is.null(data)) {
-      formula <- Formula::as.Formula(formula)
-      data <- as.data.table(eval(quote(model.frame(formula, data  = data))))
+      data <- as.data.table(eval(quote(model.frame(Formula::as.Formula(formula$formula),
+                                                   data  = data))))
    } else {
       # Check if columns are indata
-      all.cols <- c(value.var, row.vars, col.vars)
+      all.cols <- c(formula$value.var, formula$row.vars, formula$col.vars)
       missing.cols <- all.cols[!(all.cols %in% colnames(data))]
       if (length(missing.cols) != 0) {
          stop(paste0("Columns not found in data: ", paste0(missing.cols, collapse = ", ")))
       }
       data <- setDT(data)[, (all.cols), with = FALSE]
    }
-   
-   setDT(data)
-   dcast.formula <- stringr::str_squish(f[stringr::str_detect(f, "\\|")])
-   dcast.formula <- as.formula(stringr::str_replace(dcast.formula, "\\|", "~"))
-   value.var <- stringr::str_squish(f[!stringr::str_detect(f, "\\|")])
-   
-   g <- metR:::.tidy2matrix(data, dcast.formula, value.var, fill = NULL)
+   return(data)
+}
+
+lasso_fit <- function(X, y, seed) {
+   fit <- withr::with_seed(seed, glmnet::cv.glmnet(X, y, standardize = FALSE))
+   lambda <- fit$lambda.1se
+   r2 <- fit$glmnet.fit$dev.ratio[which(fit$glmnet.fit$lambda == lambda)]
+   coef <- c(as.matrix(coef(fit, s = lambda)))[-1]
+   return(list(r2 = r2, coef = coef))
+}
+
+Regression2D <- function(formula, y, data = NULL, B = 1, 
+                         method = c("cv", "lasso", "neof"),
+                         keep = 0.95, seed = 42) {
+   # Housekeeping. Getting data and transforming it to matrix
+   formula <- enrich_formula(formula)
+   data <- data_from_formula(formula = formula, 
+                             data = data)
+   g <- metR:::.tidy2matrix(data, formula$dims, formula$value.var, fill = NULL)
    
    if (length(g$matrix) < nrow(data)) {
       stop(paste("The formula ", as.character(formula), " does not identify an unique observation for each cell."))
    }
+   
    N <- length(g$rowdims[[1]])
+   if (missing(y)) y <- as.numeric(g$rowdims[[1]])
    
+   # Algo: get EOF, fit on principal components, multiply with fields to 
+   # get regression field. 
    
+   g$matrix <- scale(g$matrix, scale = FALSE)
    eof <- svd(g$matrix)
-   fit <- withr::with_seed(seed, glmnet::cv.glmnet(eof$u*sqrt(N), g$rowdims[[1]], standardize = FALSE))
-   lambda.min <<- fit$lambda.min
-   r2 <- fit$glmnet.fit$dev.ratio[which(fit$glmnet.fit$lambda == fit$lambda.min)]
-   coef <- c(as.matrix(coef(fit, s = lambda.min)))[-1]
    
-   # nonzero <- coef != 0
-   # fit <- .lm.fit(cbind(1, eof$u[, nonzero]*sqrt(N)), g$rowdims[[1]])
-   # coef <- coef(fit)[-1]
-   
-   # r2 <- 1 - var(fit$residuals)/var(g$rowdims[[1]])
-   # k <- 21
-   # eof <- svd(g$matrix, k, k)
-   # eof$d <- eof$d[1:k] 
-   # coef <- coef(.lm.fit(cbind(1, eof$u*sqrt(N)), g$rowdims[[1]]))[-1]
+   if (method == "n_eof") {
+      # TEST
+      # Limit to first EOF that explain 95% variance en be done with it. 
+      v.g <- norm(abs(g$matrix), type = "F")
+      exp_var <- eof$d^2/v.g^2
+      keep <- which(cumsum(exp_var) >= keep)[1]
+      
+      fit_function <- function(X, y, seed) {
+         zeros <- rep(0, length = ncol(X) - keep)
+         X <- X[, seq_len(keep), drop = FALSE]
+         fit <- .lm.fit(cbind(1, X), y)
+         r2 <- 1 - var(fit$residuals)/var(y)
+         
+         return(list(coef = c(coef(fit)[-1], zeros),
+                     r2 = r2))
+      }
+   } else if (method == "lasso") {
+      fit_function <- function(X, y, seed) {
+         fit <- withr::with_seed(seed, glmnet::cv.glmnet(X, y, standardize = FALSE))
+         lambda <- fit$lambda.1se
+         r2 <- fit$glmnet.fit$dev.ratio[which(fit$glmnet.fit$lambda == lambda)]
+         coef <- c(as.matrix(coef(fit, s = lambda)))[-1]
+         return(list(r2 = r2, coef = coef))
+      }
+   } else if (method == "cv") {
+      fit_function <- function(X, y, seed) {
+         K_max <- min(ncol(X), length(y))
+         mean_error <- rep(0, length = K_max)
+         sd_error <-  rep(0, length = K_max)
+         for (K in seq_len(K_max)) {
+            error <- rep(0, length = length(y))
+            for (n in seq_along(y)) {
+               fit <- .lm.fit(cbind(1, X[-n, seq_len(K), drop = FALSE]), y[-n])
+               error[n] <-  cbind(1, X[n, seq_len(K), drop = FALSE]) %*% coef(fit) - y[n]
+            }
+            mean_error[K] <- mean(error^2)
+            sd_error[K] <- sd(error^2)/sqrt(length(error))
+         }
+         
+         min_error <- which.min(mean_error)
+         
+         K <- which(mean_error[seq_len(min_error)] < (mean_error + sd_error)[min_error])[1]
+         fit <- .lm.fit(cbind(1, X[, seq_len(K), drop = FALSE]), y)
+         coef <- c(coef(fit)[-1], rep(0, length = ncol(X) - K))
+         r2 <- 1 - var(fit$residuals)/var(y)
+         
+         return(list(coef = coef,
+                     r2 = r2))
+      }
+   } else {
+      stop("unknown method")
+   }
+
+   fit <- fit_function(eof$u*sqrt(N), y, seed)
+   non_zero <- which(fit$coef != 0)
    
    E <- eof$v%*%diag(eof$d, nrow = length(eof$d))/sqrt(N)
-   coef <-  coef %*% t(E) / var(g$rowdims[[1]])
+   fit$coef <-  fit$coef %*% t(E) / var(y)   # Why divide by variance of y?
    
-   set(g$coldims, NULL, value.var, c(coef))
+   # Bootstrap
+   # if (B > 1 & any(fit$coef != 0)) {
+   #    withr::with_seed(seed, {
+   #       p.val_field <- vector("numeric", length = length(fit$coef))
+   #       p.val_r2 <- 0
+   #       for (b in seq_len(B)) {
+   #          scramble <- sample(seq_len(N), replace = TRUE)
+   #          
+   #          new_fit <- fit_function(eof$u*sqrt(N), y[scramble], seed)
+   #          new_fit$coef <-  new_fit$coef %*% t(E) / var(y) 
+   # 
+   #          
+   #          new_p <- (abs(new_fit$coef) >= abs(fit$coef))
+   #          p.val_field <- p.val_field + new_p
+   #          
+   #          p.val_r2    <- p.val_r2 + (new_fit$r2 >= fit$r2)
+   #       }
+   #       
+   #       p.val_r2 <-  p.val_r2/B
+   #       p.val_field <-  p.val_field/B
+   #    })
+   # } else {
+   #    p.val_r2 <-  NA
+   #    p.val_field <- NA
+   # }
    
-   if (B > 1) {
-      r2_null <- withr::with_seed(seed, {
-         vapply(seq_len(B), function(b) {
-            Prow <- sample(seq_len(N), replace = TRUE)
-            eof <- svd(g$matrix[Prow, ])
-            fit <- glmnet::cv.glmnet(eof$u, g$rowdims[[1]], standardize = FALSE)
-            fit$glmnet.fit$dev.ratio[which(fit$glmnet.fit$lambda == fit$lambda.min)] 
-         }, 1)})
-      r2_c <- mean(r2_null >= r2)
-   } else {
-      r2_c <- NULL
-   }
-   
-   # setattr(g$coldims, "r2", r2)
-   # setattr(g$coldims, "p.value", r2_c)
-   # set(g$coldims, NULL, "r2", c(r2))
-   # set(g$coldims, NULL, "r2_c", c(r2_c))
-   
-   # return(list(field = g$coldims,
-   #             r2 = r2,
-   #             p.value = r2_c))
-   g$coldims
+   M <- length(non_zero)
+   N <- length(y)
+   f.statistic <-  fit$r2/(1 - fit$r2)*(N - M - 1)/M
+   set(g$coldims, NULL, formula$value.var, c(fit$coef))
+   # set(g$coldims, NULL, "p.value", c(p.val_field))
+   return(
+      list(field = g$coldims,
+               non.zero = non_zero,
+               r2 = fit$r2,
+               f.statistic = f.statistic,
+               p.value = pf(f.statistic, N, N - M - 1, lower.tail = FALSE) 
+   ))
 }
-
-
-data <- ReadNetCDF("~/DATOS/NCEP Reanalysis/uwnd.mon.mean.nc", c(u = "uwnd"),
-                   subset = list(level = 300,
-                                 lat = c(0:90),
-                                 time = c("1948-01-01", "2009-12-31"))) %>% 
-   .[month(time) %in% c(12, 1, 2)] %>% 
-   .[, .(u = mean(u)), by = .(lat, lon, year(time))] 
-
-data[, u := Anomaly(u), by = .(lat, lon)]
-
-y <- unique(data$year)
-data <- data[year %in% y[1:n]]
-
-resample <- function(data) {
-   years <- sample(unique(data$year), replace = FALSE)
-   return(copy(data)[, year := years, by = .(lat, lon)])
-}
-
-point_regression <- data[, FitLm(u, year, se = TRUE), by = .(lon, lat)]
-
-ggplot(point_regression[term == "year"], aes(lon, lat)) +
-   geom_contour_fill(aes(z = estimate*10)) +
-   geom_world +
-   scale_fill_divergent("Trend in U(300hpa)", 
-                        limits = c(-2, 2)) +
-   coord_quickmap() +
-   metR:::theme_field()
-
-
-
-data[, Regression2D(u ~ year | lat + lon)] %>% 
-   ggplot(aes(lon, lat)) +
-   geom_contour_fill(aes(z = u*10*1.21)) +
-   geom_world +
-   scale_fill_divergent("Trend in U(300hpa)", 
-                        limits = c(-2, 2)) +
-   coord_quickmap() +
-   metR:::theme_field()
-
-
-data[, FitLm(u, year, se = TRUE), by = .(lon, lat)] %>% 
-   .[term == "year"] %>% 
-   .[data[, Regression2D(u ~ year | lat + lon)], on = c("lon", "lat")] %>% 
-   ggplot(aes(u, estimate)) +
-   geom_point() +
-   geom_abline() +
-   coord_fixed()
-
-
-
-y <- unique(data$year)
-new_data <- data[year %in% y[1:n]]
-ratio <- unlist(lapply(10:N, function(n) {
-   new_data <- data[year %in% y[1:n]]
-   new_data[, FitLm(u, year, se = TRUE), by = .(lon, lat)] %>% 
-      .[term == "year"] %>% 
-      .[new_data[, Regression2D(u ~ year | lat + lon)$field], on = c("lon", "lat")] %>% 
-      lm(u ~ estimate, data = .) %>% 
-      coef() %>% 
-      .[["estimate"]]   
-}
-
-))
-
-ggplot(data.frame(N = 10:N, ratio = ratio), aes(N^3, ratio)) +
-   geom_point() +
-   geom_smooth(method = "lm")
-
-coef(lm(ratio ~ I(N^3) - 1, data = data.frame(N = 10:N, ratio = ratio)))
-
-
-B <- 100
-fdr_naive <- unlist(lapply(1:B, function(x) {
-   resample(data) %>% 
-      .[, FitLm(u, year, se = TRUE), by = .(lon, lat)] %>% 
-      .[term == "year"] %>%
-      .[, mean(abs(estimate)/std.error > 2)]
-}))
-
-data <- resample(data)
-
-data %>% 
-   .[, FitLm(u, year, se = TRUE), by = .(lon, lat)] %>% 
-   .[term == "year"] %>%
-   ggplot(aes(lon, lat)) +
-   geom_contour_fill(aes(z = estimate*10)) +
-   geom_world +
-   scale_fill_divergent() +
-   coord_quickmap()
-
-data %>% 
-   .[, Regression2D(u ~ year | lon + lat)] %>% 
-   ggplot(aes(lon, lat)) +
-   geom_contour_fill(aes(z = u*10)) +
-   geom_world +
-   scale_fill_divergent() +
-   coord_quickmap()
-
-
-
-r2_eof <- unlist(lapply(1:B, function(x) {
-   resample(data) %>% 
-      Regression2D(u ~ year | lon + lat, data = ., B = 100) %>% 
-      attr("p.value")
-}))
 
