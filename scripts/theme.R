242895e7a16f20b24bdf742daaacd698b38df9b1
eliocamp <eliocampitelli@gmail.com>
2021-10-19 15:10:22 -0300

    Comparación con CMIP6

---
+++ scripts/theme.R
@@ -0,0 +1,19 @@
+theme_elio <- function(base_size = 16) {
+   ggplot2::theme_minimal(base_size = base_size) +
+      ggplot2::theme(
+         # strip.background = ggplot2::element_rect(fill = NA, color = "gray30"),
+         # text = element_text(family = font_rc),
+         legend.position = "bottom", legend.box = "vertical",
+         tagger.panel.tag.background = ggplot2::element_rect(color = NA),
+         strip.text = ggplot2::element_text(size =  ggplot2::rel(11/16)),
+         # panel.spacing.y = grid::unit(5, "mm"),
+         # panel.spacing.x = grid::unit(5, "mm"),
+         # legend.spacing = grid::unit(2, "mm"),
+         # panel.border = ggplot2::element_rect(colour = "black", fill = NA),
+         # plot.margin = grid::unit(rep(3, 4), "mm"),
+         # # legend.title = element_blank(),
+         # legend.box.spacing = grid::unit(3, "mm"),
+         # legend.margin = ggplot2::margin(t = -5),
+         panel.grid = ggplot2::element_line(color = scales::alpha("gray60", 0.5), size = 0.1),
+         panel.ontop = TRUE)
+}
