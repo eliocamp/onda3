110df0987c2e3a387eed412e257a7ba8d7f9b59c
eliocamp <eliocampitelli@gmail.com>
2023-01-26 13:08:25 -0300

    Arregla un poco la cache

--- render_cmip6.R
+++ render_cmip6.R
@@ -7,7 +7,7 @@ for (trimestre in trimestres) {
    message("Renderizando ", trimestre)
    rmarkdown::render("35-cEOF-CMIP6-superensemble.Rmd", 
                      output_file = paste0("35-cEOF-CMIP6-superensemble-", trimestre),
-                     params = list(season = trimestre))
+                     params = list(season = trimestre, cache.extra = trimestre))
 }
 
 message("Listo!")
